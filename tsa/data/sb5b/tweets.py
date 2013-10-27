9da50045cc9d67df8d8d075a6e2a2dc7e9f137ee
Christopher Brown <io@henrian.com>
2013-10-27 17:31:52 -0500

    Add specific iterable-like pickling handler for sb5b tweet data

--- tsa/data/sb5b/tweets.py
+++ tsa/data/sb5b/tweets.py
@@ -2,6 +2,10 @@
 import os
 from tsa.lib import tabular, html
 
+import logging
+logger = logging.getLogger(__name__)
+
+
 xlsx_filepath = '%s/ohio/sb5-b.xlsx' % os.getenv('CORPORA', '.')
 label_keys = ['For', 'Against', 'Neutral', 'Broken Link', 'Not Applicable']
 
@@ -17,3 +21,23 @@ def read(limit=None):
         row['Tweet'] = html.unescape(row['Tweet'])
 
         yield row
+
+
+def read_cached(limit=None):
+    import cPickle as pickle
+    pickle_filepath = '%s.pickled-%s' % (xlsx_filepath, limit or 'all')
+
+    if os.path.exists(pickle_filepath):
+        logger.info('Loading pickled sb5b tweets file from %s', pickle_filepath)
+        pickle_file = open(pickle_filepath, 'rb')
+        for item in pickle.load(pickle_file):
+            yield item
+    else:
+        logger.info('Reading fresh sb5b tweets')
+        items = list(read(limit=limit))
+
+        logger.info('Pickling sb5b tweets to %s', pickle_filepath)
+        pickle_file = open(pickle_filepath, 'wb')
+        pickle.dump(items, pickle_file)
+        for item in items:
+            yield item
