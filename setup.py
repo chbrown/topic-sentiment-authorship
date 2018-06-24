7859ddb185f04f4e73bb1e788a50b996b14bc0b6
Christopher Brown <io@henrian.com>
2018-06-24 11:33:10 -0500

    Move deps to requirements.txt

--- setup.py
+++ setup.py
@@ -13,15 +13,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     include_package_data=True,
     zip_safe=False,
-    install_requires=[
-        'gensim == 0.8.6',
-        'justext == 2.0.0',
-        'openpyxl == 1.6.2',
-        'psycopg2 == 2.5.1',
-        'requests == 1.2.3',
-        'sqlalchemy == 0.8.2',
-        'scikit-learn == 0.14.1',
-    ],
+    install_requires=open('requirements.txt').readlines(),
     entry_points={
         'console_scripts': [
             'tsa-hashtags=tsa.cli.hashtags:main',
