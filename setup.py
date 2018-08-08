2c58f356ad01390c4ecdb564d2d9eb10dbc52ebc
Christopher Brown <io@henrian.com>
2018-08-08 12:35:13 -0500

    Specify setup package(s) explicitly

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import json
-import setuptools
+from setuptools import setup
 
 package = json.load(open('package.json'))
 
-setuptools.setup(
+setup(
     name=str(package['name']),
     version=str(package['version']),
     url=str(package['homepage']),
     author=str(package['author']['name']),
     author_email=str(package['author']['email']),
     license=open('LICENSE').read(),
-    packages=setuptools.find_packages(),
+    packages=['tsa'],
     include_package_data=True,
     zip_safe=False,
     install_requires=open('requirements.txt').readlines(),
